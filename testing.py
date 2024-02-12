5a2ade751c7a66fc09794f42a83529f230203a9b
juicewar <joshua.senoron.23@ucl.ac.uk>
2024-02-12 15:51:05 +0000

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,13 +1,11 @@
 from fhirclient import client
+import fhirclient.models.patient as p
+
 settings = {
     'app_id': 'my_web_app',
-    'api_base': 'https://fhir-open-api-dstu2.smarthealthit.org'
+    'api_base': 'https://r3.smarthealthit.org'
 }
 smart = client.FHIRClient(settings=settings)
 
-import fhirclient.models.patient as p
-patient = p.Patient.read('hca-pat-1', smart.server)
-patient.birthDate.isostring
-# '1963-06-12'
-smart.human_name(patient.name[0])
-# 'Christy Ebert'
+patient = p.Patient.read("291112", smart.server)
+print(patient.as_json())
